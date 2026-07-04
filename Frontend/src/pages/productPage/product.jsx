462087c86fabeca0c0029e100c6b8037ce24516a
shanbel <shanbeldires7@gmail.com>
2026-07-04 20:44:04 +0300

    add object of product, send product list to backend and make input fields set data t

--- Frontend/src/pages/productPage/product.jsx
+++ Frontend/src/pages/productPage/product.jsx
@@ -2,23 +2,72 @@ import react from "react";
 import axios from "axios"
 import {useState,useEffect} from "react-dom"
 export function Product(){
-    const[product,setProduct]=useState([]);
+    const[product,setProduct]=useState({
+        name:"",
+        price:0,
+        description:"",
+        category:"",
+        image:"",
+        stock:0
+    });
     useEffect(()=>{
-        axios.post("http://localhost:5000/api/v1/products",{product})
+        fetch("http://localhost:5000/api/v1/products",{
+            methode:"POST",
+            headers:{
+                "content-type":"application/json"
+            },
+            body:product
+        })
     },[product])
+
     function handler(e){
         e.preventDefault();
-        setProduct()
     }
     return(
         <>
-            <input type="text" name="" id="" />
-            <input type="text" name="" id="" />
-            <input type="text" name="" id="" />
-            <input type="text" name="" id="" />
-            <input type="text" />
-            <input type=""/>
-            <button>add</button>
+            <input 
+                type="text" 
+                name="name" 
+                id=""
+                value={product.name}
+                onChange={e=>setProduct(e.target.value)}
+             />
+            <input 
+                type="Number" 
+                name="price" 
+                id="" 
+                value={product.price}
+                onChange={e=>setProduct(e.target.value)}
+             />
+            <input 
+                type="text" 
+                name="description"
+                id="" 
+                value={product.description}
+                onChange={e=>setProduct(e.target.value)}
+             />
+            <input 
+                type="text" 
+                name="category" 
+                id="" 
+                value={product.category}
+                onChange={e=>setProduct(e.target.value)}
+            />
+            <input 
+                type="image" 
+                name="image"
+                id=""  
+                value={product.image}
+                onChange={e=>setProduct(e.target.value)}
+            />
+            <input 
+                type="Number" 
+                name="stock"
+                id="" 
+                value={product.stock}
+                onChange={e=>setProduct(e.target.value)}
+            />
+            <button onClick={handler}>add</button>
         </>
     )
 }
