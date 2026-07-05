3c64aaff45e3ca39e7c06be7f7c4adb29e807b37
shanbel <shanbeldires7@gmail.com>
2026-07-05 20:48:17 +0300

    add opder page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Order } from './pages/orderPage/order'
 import { Product } from './pages/productPage/product'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
         <Product/>
+        <Order/>
     </>
   )
 }
