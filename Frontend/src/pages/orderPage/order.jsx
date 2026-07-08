05560466cbe11ecced3d04d671a91494a632c8d7
shanbel <shanbeldires7@gmail.com>
2026-07-08 12:35:27 +0300

    adding onchange function to handle onchange attributes and also delete await

--- Frontend/src/pages/orderPage/order.jsx
+++ Frontend/src/pages/orderPage/order.jsx
@@ -13,10 +13,15 @@ export function Order(){
     });
     const[error,setError]=useState("")
     const[message,setMessage]=useState("")
-    
+    function onchange(e){
+        const{name,value} = e.target;
+        setOrder({
+            ...order,[name]:value
+        })
+    }
      async function handleSubmit(e){ 
         e.preventDefault();
-        await  fetch("http://localhost:5000/api/v1/orders",{
+        fetch("http://localhost:5000/api/v1/orders",{
                 method:"POST",
                 headers:{
                     "content-type":"application/json"
@@ -35,34 +40,34 @@ export function Order(){
     return(
         <div className="order">
            <form onSubmit={handleSubmit} className="order-form">
-                 <div className="order-input">
-                <input 
-                    type="text" 
-                    name="customerName" 
-                    id=""
-                    value={order.customerName}
-                    onChange={e=>setOrder(e.target.value)}
-                />
-            </div>
-            <div className="order-input">
-                <input 
-                    type="text" 
-                    name="customerAddress"
-                    id="" 
-                    value={order.customerAddress}
-                    onChange={e=>setOrder(e.target.value)}
-                /> 
-            </div>
+                <div className="order-input">
+                    <input 
+                        type="text" 
+                        name="customerName" 
+                        id=""
+                        value={order.customerName}
+                        onChange={onchange}
+                    />
+                </div>
+                <div className="order-input">
+                    <input 
+                        type="text" 
+                        name="customerAddress"
+                        id="" 
+                        value={order.customerAddress}
+                        onChange={onchange}
+                    /> 
+                </div>
 
-            <div className="order-button">
-                <button >
-                    place your order
-                </button>
-            </div>
-            <div className="order-message">
-               {message && <p className="message">{message}</p>}
-                {error && <p className="error">{error}</p>}
-            </div>
+                <div className="order-button">
+                    <button >
+                        place your order
+                    </button>
+                </div>
+                <div className="order-message">
+                {message && <p className="message">{message}</p>}
+                    {error && <p className="error">{error}</p>}
+                </div>
            </form> 
         </div>
     )
