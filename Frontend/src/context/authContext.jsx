2e86d88b47db0ceed34bb33a024b31cba2872256
shanbel <shanbeldires7@gmail.com>
2026-07-09 09:30:47 +0300

    adding auth provider function, use context and create context states

--- Frontend/src/context/authContext.jsx
+++ Frontend/src/context/authContext.jsx
@@ -1,22 +1,43 @@
-import React from "react"
-import {AuthApi} from "../lib/api.jsx"
+import React, { createContext, useContext, useState } from "react"
+import {AuthApi, GetAccessToken} from "../lib/api.jsx"
 
-export async function Register(credentials){
-    const response= await AuthApi.register(credentials);
-    return response.json();
+export function AuthProvider({children}){
+    const authContext = createContext();
+    const [isAuthenticate,setIsAuthenticate] = Boolean(GetAccessToken())
+    const []
+    export async function Register(credentials){
+        const response= await AuthApi.register(credentials);
+        return response.json();
+    }
+
+    export async function Login(credentials){
+        const response= await AuthApi.login(credentials);
+        return response.json();
+    }
+
+    export async function LogOut(){
+        await AuthApi.logout();
+        ClearToken();
+    }
+
+    export async function Profile(){
+        const response= await AuthApi.profile();
+        return response.json();
+    }
+    <authContext.provider value={
+        Login,
+        Register,
+        LogOut,
+        Profile,
+        isAuthenticate
+    }>
+
+    </authContext.provider>
 }
-
-export async function Login(credentials){
-    const response= await AuthApi.login(credentials);
-    return response.json();
-}
-
-export async function LogOut(){
-    await AuthApi.logout();
-    ClearToken();
-}
-
-export async function Profile(){
-    const response= await AuthApi.profile();
-    return response.json();
+export function UseAuth(){
+    const auth =useContext(authContext)
+    if(!auth){
+        return "auth not found"
+    }
+    return auth
 }
