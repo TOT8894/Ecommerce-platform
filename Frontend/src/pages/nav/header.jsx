cfe0f4b2bcc5a70932500741a7ce85f696c8c0cf
shanbel <shanbeldires7@gmail.com>
2026-07-09 11:02:30 +0300

    adding header navigation links

---
+++ Frontend/src/pages/nav/header.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom"
+export function Header(){
+    return(
+        <>
+        <Link to="register"> register</Link>
+        <Link to="login"> register</Link>
+        <Link to="profile"> register</Link>
+        <Link to="logout"> register</Link>
+        </>
+    )
+}
